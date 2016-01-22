c7b292fd3535aabaa20611b54d6017e8adc3a62e
Falko Janak <contact@falkojanak.com>
2016-01-22 11:21:56 +0100

    initial commit

---
+++ HelloWorldLib/HelloWorldLib/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorldLib
+{
+    public class HelloWorld
+    {
+        public override string ToString()
+        {
+            return "Hello World";
+        }
+    }
+}
